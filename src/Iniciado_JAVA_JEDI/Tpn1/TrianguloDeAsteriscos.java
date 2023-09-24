957fc60f59386894b2954a56aae6f74165f8621e
LDA407 <88114886+LDA407@users.noreply.github.com>
2023-09-24 18:25:15 -0300

    Update TrianguloDeAsteriscos.java

--- src/Iniciado_JAVA_JEDI/Tpn1/TrianguloDeAsteriscos.java
+++ src/Iniciado_JAVA_JEDI/Tpn1/TrianguloDeAsteriscos.java
@@ -1,6 +1,8 @@
-package Iniciado_JAVA_JEDI.Tpn1;
-
 public class TrianguloDeAsteriscos {
+    /**
+     * Se le solicita que creen un programa que imprima un triángulo de asteriscos
+     * en la consola.
+     */
     public static void triangle_of_asterisks_without_for() {
         System.out.println("*");
         System.out.println("**");
