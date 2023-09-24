1d81245ed7d54e14da2800df5e72ca4a7510f915
LDA407 <88114886+LDA407@users.noreply.github.com>
2023-09-24 18:11:07 -0300

    Create TrianguloDeAsteriscos.java

---
+++ src/Iniciado_JAVA_JEDI/Tpn1/TrianguloDeAsteriscos.java
@@ -0,0 +1,12 @@
+package Iniciado_JAVA_JEDI.Tpn1;
+
+public class TrianguloDeAsteriscos {
+    public static void triangle_of_asterisks_without_for() {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
