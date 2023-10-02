28f2598cdd19755aec27a4bdc3008ad5bc7582bd
lda407 <davidlencinaok@gmail.com>
2023-10-02 18:34:19 -0300

    ConversionDeBitcoinsADolares

--- src/Caballero_JAVA_JEDI/Tpn1/ConversionDeBitcoinsADolares.java
+++ src/Caballero_JAVA_JEDI/Tpn1/ConversionDeBitcoinsADolares.java
@@ -2,10 +2,10 @@ package Caballero_JAVA_JEDI.Tpn1;
 
 public class ConversionDeBitcoinsADolares {
   /*
-    Escribir un programa en Java que solicite al usuario una cantidad en bitcoins
-    y muestre el equivalente en dólares, utilizando una tasa de cambio fija. Por
-    ejemplo, si la tasa de cambio es de 1 bitcoin = 50000 dólares, y el usuario
-    ingresa 0.5 bitcoins, el programa deberá mostrar 25000 dólares.
+   * Escribir un programa en Java que solicite al usuario una cantidad en bitcoins
+   * y muestre el equivalente en dólares, utilizando una tasa de cambio fija. Por
+   * ejemplo, si la tasa de cambio es de 1 bitcoin = 50000 dólares, y el usuario
+   * ingresa 0.5 bitcoins, el programa deberá mostrar 25000 dólares.
    */
 
   Double tasaDeCambio = 27025.77;
@@ -14,7 +14,8 @@ public class ConversionDeBitcoinsADolares {
 
   public void conversion() {
     System.out.println(
-      String.format("Tus %s equivalen a %d$.", this.bitcoins, this.dolares)
+        String.format("Tus %s equivalen a %d$.", this.bitcoins, this.dolares)
     );
   }
+
 }
