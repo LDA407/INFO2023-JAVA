af3e3d0e5a124e60861204d9a37a4266f60cb662
lda407 <davidlencinaok@gmail.com>
2023-10-02 06:16:42 -0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 import java.util.*;
 
 import Caballero_JAVA_JEDI.Tests.TestTpn2;
+import Caballero_JAVA_JEDI.Tpn1.ConversionDeDolaresAPesos;
 import Iniciado_JAVA_JEDI.Tpn2.*;
 import Iniciado_JAVA_JEDI.Tpn1.*;
 import Caballero_JAVA_JEDI.Tpn2.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] Float) {
         MyFriends.Lista_de_amigos();
         TriangleOfAsterisks.triangle_of_asterisks(34);
         TrianguloDeAsteriscos.triangle_of_asterisks_without_for();
+        ConversionDeDolaresAPesos.dolarOficial();
+        ConversionDeDolaresAPesos.dolarMayorista();
+        ConversionDeDolaresAPesos.dolarMinorista();
+        ConversionDeDolaresAPesos.dolarAgro();
+        ConversionDeDolaresAPesos.dolarBlue();
+        ConversionDeDolaresAPesos.dolarColdplay();
+        ConversionDeDolaresAPesos.dolarSoja();
+        ConversionDeDolaresAPesos.dolarQatar();
     }
 
 }
