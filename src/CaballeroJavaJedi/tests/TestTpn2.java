18eb2aa1adc8b3bce14144473d8ad792eacb3ad3
David Alejandro Lencina <88114886+LDA407@users.noreply.github.com>
2023-10-11 23:49:20 -0300

    Create TestTpn2.java

---
+++ src/CaballeroJavaJedi/tests/TestTpn2.java
@@ -0,0 +1,10 @@
+package CaballeroJavaJedi.tests;
+
+import CaballeroJavaJedi.trabajoPractico2.*;
+
+public class TestTpn2 {
+    public static void main(String[] args) {
+        DameLosMayoresYLosMenores.dame_el_minimo();
+        DameLosMayoresYLosMenores.dame_el_maximo();
+    }
+}
