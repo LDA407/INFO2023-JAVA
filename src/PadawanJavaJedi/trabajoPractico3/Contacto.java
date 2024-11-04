c7266575b24091690c155fcd659a31ba4db4b423
David Alejandro Lencina <88114886+LDA407@users.noreply.github.com>
2024-11-04 16:38:33 -0300

    Actualizar Contacto.java

--- src/PadawanJavaJedi/trabajoPractico3/Contacto.java
+++ src/PadawanJavaJedi/trabajoPractico3/Contacto.java
@@ -1,2 +1,46 @@
-package PadawanJavaJedi.trabajoPractico3;public class Contacto {
+package PadawanJavaJedi.trabajoPractico3;
+
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String correoElectronico;
+
+    public Contacto(String nombre, String telefono, String correoElectronico) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correoElectronico='" + correoElectronico + '\'' +
+                '}';
+    }
 }
